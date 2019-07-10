99a403eadbb662a1190c649ee453b3eaf8980b70
tripl3a <triple_a@mailbox.org>
2019-07-10 17:05:09 +0200

    switched to German BERT

--- hate_detector/tlhd_classifier_training.sh
+++ hate_detector/tlhd_classifier_training.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
 export TASK_NAME=TLHD
+#export DATA_DIR=~/git-reps/transfer-learning-for-hatespeech-detection/data/modeling/gutefrage-q-del
+#export OUT_DIR=/tmp
 export DATA_DIR=/home/aallhorn/data
 export OUT_DIR=/home/aallhorn/output
 
@@ -10,7 +12,7 @@ python ../examples/run_classifier.py \
   --output_dir $OUT_DIR/$TASK_NAME \
   --do_train \
   --do_eval \
-  --bert_model bert-base-multilingual-cased \
+  --bert_model bert-base-german-cased \
   --max_seq_length 128 \
   --train_batch_size 32 \
   --learning_rate 2e-5 \
